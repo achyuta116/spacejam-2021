2e01ef02850b50c2b2587f02cae021c819629131
Achyuta <achyutathegreat@gmail.com>
2021-02-27 23:06:36 +0530

    Database update

--- database.py
+++ database.py
@@ -20,13 +20,18 @@ def get_cards(deck_name):
 
 def get_learn_cards(deck_name, learn_limit):
     return c.execute("""SELECT * FROM {} WHERE mode="learn" LIMIT {}""".format(deck_name, learn_limit))
-# def get_review_cards(deck_name,review_limit):
-#     cards = []
-#     rows = c.execute("""SELECT * FROM {} WHERE mode="review" """.format(deck_name,review_limit))
-#     for i in rows:
-#         date_ = i.
 
+def get_review_cards(deck_name,review_limit):
+    cards = []
+    rows = c.execute("""SELECT * FROM {} WHERE mode="review" """.format(deck_name))
+    for i in rows:
+        date = i['date']
+        if date < datetime.date.today():
+            cards.append(i)
+    return cards
 
+def list_decks():
+    return c.execute("""SELECT name FROM sqlite_master WHERE type='table'""")
 def intervals(current_intv, current_EF):
     next_intv = []
     for i in range(5):
@@ -45,11 +50,11 @@ def update_card(deck_name, question, new_interval, new_EF):
     c.execute("""UPDATE {} set current_interval={}, current_EF={}, next_date={}, mode="review" WHERE question={}""".format(
         deck_name, new_interval, new_EF, question, str(new_date)))
 
-
 def new_EF_calculation(current_EF, q):
     return current_EF+0.1-(5-q)*(0.08+(5-q)*0.02)
 
+def delete_deck(deck_name):
+    c.execute("""DROP TABLE {}""".format(deck_name))
 
 def delete_card(deck_name, question):
     c.execute("DELETE FROM {} WHERE question={}".format(deck_name, question))
-
