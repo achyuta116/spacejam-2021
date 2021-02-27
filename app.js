563221ba4ac2cb527c31f351d1c474540ffebffb
Achyuta <achyutathegreat@gmail.com>
2021-02-27 13:09:11 +0530

    What I'm working on slowly

---
+++ app.js
@@ -0,0 +1,7 @@
+const answerToggle = () => {
+    const ansCard = document.getElementById("answer");
+    const questionCard = document.getElementById("question");
+    const showAnsButton =  document.getElementById("show-intervals-button");
+    const ansButtonsArray = document.querySelectorAll(".interval-button");
+    
+}
