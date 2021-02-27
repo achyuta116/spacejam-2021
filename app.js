24d54d67f203beb35462d10cfcec779b13e82739
Achyuta <achyutathegreat@gmail.com>
2021-02-27 13:43:42 +0530

    Javascript file changes along with minor changes to html and css files

--- app.js
+++ app.js
@@ -3,5 +3,19 @@ const answerToggle = () => {
     const questionCard = document.getElementById("question");
     const showAnsButton =  document.getElementById("show-intervals-button");
     const ansButtonsArray = document.querySelectorAll(".interval-button");
-    
+    const buttons = document.querySelectorAll(".button");
+    buttons.forEach((item,index) => {
+        console.log(index);
+        item.addEventListener("click",()=>{
+           ansCard.classList.toggle("toggle");
+           questionCard.classList.toggle("toggle");
+        });
+    });
+    ansButtonsArray.forEach((item,index)=>{
+        
+        item.addEventListener("click",()=>{
+            //Add the code to update the intervals in database by passing "index" in the function you're using
+        });
+    });
 }
+answerToggle();
